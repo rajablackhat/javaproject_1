bbc13cf86ee592bfcef7c73850f165671dbf602f
Raja <47786444+rajablackhat@users.noreply.github.com>
2026-06-15 21:09:05 +0530

    Fix capitalization in welcome message

--- src/Welcome.java
+++ src/Welcome.java
@@ -17,7 +17,7 @@ class Welcome
 	public static void main(String args[])
 	{
 
-		String message ="\n Welcome to Java";
+		String message ="\n Welcome to java";
 		String member = "Raja";
 		WelcomeProto wp =new WelcomeProto();
 		wp.display(message);
